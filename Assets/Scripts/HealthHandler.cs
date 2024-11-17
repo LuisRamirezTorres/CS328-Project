407a92a2d0bee329fdf930cdf1116c72b5915762
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-11-16 19:02:50 -0800

    basic healthbar, need to attach script to HUD game object

---
+++ Assets/Scripts/HealthHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HealthHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private VisualElement bar;
+    public int health = 100;
+    void Start()
+    {
+        UIDocument ui_doc = gameObject.GetComponent<UIDocument>();
+        VisualElement ve = ui_doc.rootVisualElement;
+        bar = ve.Q(className: "unity-progress-bar__progress");
+        bar.style.backgroundColor = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIDocument ui_doc = gameObject.GetComponent<UIDocument>();
+        VisualElement ve = ui_doc.rootVisualElement;
+        bar = ve.Q(className: "unity-progress-bar__progress");
+        bar.style.backgroundColor = Color.red;
+        ProgressBar pb = (ProgressBar)ve.Q(className: "unity-progress-bar");
+        pb.value = health;
+    }
+}
