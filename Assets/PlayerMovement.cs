f5b825363f8f6b14eb8957e4fa6ea47c23a36c42
Chito7 <luisrt720@gmail.com>
2024-11-17 17:15:07 -0800

    Combat, Movement/Controls, Animations
    Implemented working combat mechanic (punch), animations, and character controller/movement scripts.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Animator anim;
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+
+    public GameObject punchPoint;
+    public GameObject runPunchPoint;
+    public float radius;
+    public LayerMask enemies; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        anim.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            anim.SetBool("isJumping", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetBool("isPunching", true);
+        }
+
+    }
+
+    public void onLanding()
+    {
+        anim.SetBool("isJumping", false);
+    }
+
+
+    public void punch()
+    {
+        Collider2D[] enemy = Physics2D.OverlapCircleAll(punchPoint.transform.position, radius, enemies);
+    
+        foreach (Collider2D enemyGameObject in enemy) {
+            Debug.Log("Enemy Hit");
+        }
+    }
+
+    public void runPunch()
+    {
+        Collider2D[] enemy = Physics2D.OverlapCircleAll(runPunchPoint.transform.position, radius, enemies);
+
+        foreach (Collider2D enemyGameObject in enemy) {
+            Debug.Log("Enemy Hit");
+
+        }
+    }
+
+
+    public void endPunch()
+    {
+        anim.SetBool("isPunching", false);
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);    // Move our player
+        jump = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(punchPoint.transform.position, radius);
+        Gizmos.DrawWireSphere(runPunchPoint.transform.position, radius);
+    }
+
+}
