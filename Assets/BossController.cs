15de4047356e8e64756b099227c1ff64a5b5933a
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-11-21 22:52:35 -0800

    tested successful merge

---
+++ Assets/BossController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ProjectileBehavior projectile;
+    public Transform offset;
+    private float cooldown = 3.0f;
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = transform.localScale;
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1;
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0)
+        {
+            cooldown = 3.0f;
+            Instantiate(projectile, offset.position, transform.rotation);
+        }
+    }
+}
