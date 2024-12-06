2c957f2d0b17c95cd7fa6a1cf7034f93021f92ac
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-12-05 23:55:10 -0800

    Added new enemy, fixed bullet collision issue

--- Assets/ExplosionBehavior.cs
+++ Assets/ExplosionBehavior.cs
@@ -2,29 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RobotBullet : MonoBehaviour
+public class ExplosionBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject player;
-    private Rigidbody2D rb;
-    public float force;
-    private float timer;
+    private float timer = 0.0f;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        Vector3 dir = player.transform.position - transform.position;
-        rb.velocity = new Vector2(dir.x, dir.y).normalized * force;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if ( timer > 10)
+        if (timer > 0.6f )
         {
             Destroy(gameObject);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject hud = GameObject.FindGameObjectWithTag("HealthHUD");
+            hud.GetComponent<HealthHandler>().health -= 5;
+
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,8 +36,8 @@ public class RobotBullet : MonoBehaviour
         {
             GameObject hud = GameObject.FindGameObjectWithTag("HealthHUD");
             hud.GetComponent<HealthHandler>().health -= 5;
-            Destroy(gameObject);
 
         }
     }
+
 }
