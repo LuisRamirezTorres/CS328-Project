4fd94d301db081afed29a8f66ba9514da20e236f
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-12-06 00:26:42 -0800

    tested changes, working well, added enemies to lvl1

--- Assets/RobotBullet.cs
+++ Assets/RobotBullet.cs
@@ -33,8 +33,8 @@ public class RobotBullet : MonoBehaviour
         {
             GameObject hud = GameObject.FindGameObjectWithTag("HealthHUD");
             hud.GetComponent<HealthHandler>().health -= 5;
-            Destroy(gameObject);
 
         }
+        Destroy(gameObject);
     }
 }
